e3f02b02cfe23ea45020353c8a2ec116f2b85652
devwicca <devwicca@gmail.com>
2019-04-14 09:42:12 +0300

    fixed discord icon and side bar

--- client/src/components/Profile/FooterSection.js
+++ client/src/components/Profile/FooterSection.js
@@ -11,7 +11,7 @@ const FooterSection = () => (
         <Row className="center ">
             <Col l={4} s={4}><li><a target="_blank" rel="noopener noreferrer" href="https://github.com/DevWicca"                    className="btn-floating btn-small black "><i className="fab fa-github fa-2x  white-text"></i></a><b>Github</b></li></Col>
             <Col l={4} s={4}><li><a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/walid-mostafa-513b99182" className="btn-floating btn-small black "><i className="fab fa-linkedin fa-2x white-text"></i></a><b>Linkedin</b></li></Col>
-            <Col l={4} s={4}><li><a target="_blank" rel="noopener noreferrer" href="https://discord.gg/DR3YNu" className="btn-floating btn-small black "><i className="fab fa-trello fa-2x white-text"></i></a><b>Discord</b></li></Col>
+            <Col l={4} s={4}><li><a target="_blank" rel="noopener noreferrer" href="https://discord.gg/DR3YNu" className="btn-floating btn-small black "><i className="fab fa-discord fa-2x white-text"></i></a><b>Discord</b></li></Col>
         </Row>
             </ul>}
         className="black center">
